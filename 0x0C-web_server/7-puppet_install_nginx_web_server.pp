a80868c25bfaf64212649f4b880b63520ad62ada
BoazAkunga <boazakunga@gmail.com>
2024-08-15 19:56:57 -0700

    recovered

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# automating configuration with puppet
+
+package { 'nginx':
+	ensure => installed,
+}
+
+file line { 'install':
+	ensure => 'present',
+	path => '/etc/nginx/sites-available/default',
+	after => 'listen 80 default_server;;,
+	line => 'rewrite ^/redirect_me https://www.youtube.com/@globaltechhub permanently;',
+}
+
+file { '/var/www/html/index.html':
+	content => 'Hello World!',
+}
+
+service { 'nginx':
+	ensure => running,
+	require => package['nginx'],
+}
